aacae2696249e7cf0f4fcdce5a2f60cf0133afcb
shah2801 <shahnazz2801@gmail.com>
2026-07-11 12:13:54 +0530

    Initial commit for portfolio project

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links a");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+const roles = [
+    "IT Support Engineer",
+    "Cloud Enthusiast",
+    "Application Support Engineer",
+    "Full Stack Developer"
+];
+
+const changingText = document.getElementById("changing-text");
+
+let index = 0;
+
+setInterval(() => {
+
+    changingText.style.animation = "none";
+    changingText.offsetHeight; // Restart animation
+    changingText.style.animation = "textSlide .5s ease";
+
+    index = (index + 1) % roles.length;
+    changingText.textContent = roles[index];
+
+}, 2500);
